0a0c2a5a0db45273006ee1c83d5ba0c624f8c701
junnubabu-ctrl <junnubabu@pondiuni.ac.in>
2026-09-10 14:41:05 +0530

    Expose EviTrust execution API

---
+++ src/paper4_kbvqa/execution/__init__.py
@@ -0,0 +1,15 @@
+from .controller import (
+    EviTrustExecutionController,
+    ExecutionConfig,
+    leakage_safe_query,
+    provenance_score,
+    reliability_fusion,
+)
+
+__all__ = [
+    "EviTrustExecutionController",
+    "ExecutionConfig",
+    "leakage_safe_query",
+    "provenance_score",
+    "reliability_fusion",
+]
